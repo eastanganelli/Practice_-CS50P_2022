9c80fe47d198ba539cafc9722d7a4028302b1806
Ezequiel Augusto Stanganelli <ezequielstanganelli@gmail.com>
2022-06-24 03:45:20 +0000

    First Chapter

--- CH_1/hello.py
+++ CH_1/hello.py
@@ -3,5 +3,9 @@ from tokenize import Name
 
 name = input("What's your name? ")
 
-#print("Hello, " + name)
-print("Hello, ", name)
+# printf can be use as console.log() to print multiple variables
+# +: only for strings?
+# print has a similar param print with f"{variable}" (js: ${variable})
+
+#print("Hello, " + name) 
+print("Hello, ", name) 
