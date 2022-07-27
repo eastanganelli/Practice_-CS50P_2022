b68bb95a0bb9bdb45197654fe944354c09815122
Ezequiel Augusto Stanganelli <ezequielstanganelli@gmail.com>
2022-07-27 22:49:46 +0000

    New notes

--- src0/my_notes/hello.py
+++ src0/my_notes/hello.py
@@ -1,6 +1,3 @@
-from tokenize import Name
-
-
 name = input("What's your name? ")
 
 # printf can be use as console.log() to print multiple variables
@@ -8,4 +5,5 @@ name = input("What's your name? ")
 # print has a similar param print with f"{variable}" (js: ${variable})
 
 #print("Hello, " + name) 
-print("Hello, ", name) 
+print("Hello, ", name)
+
