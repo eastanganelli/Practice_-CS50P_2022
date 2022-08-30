96a9003a6e34bd6509c1a664b3b0e342fc4b1a48
Ezequiel Augusto Stanganelli <ezequielstanganelli@gmail.com>
2022-08-30 14:02:31 +0000

    3 excercises finished

---
+++ cs50/problems/2022/python/problem_set3/fuel/fuel.py
@@ -0,0 +1,31 @@
+import re
+from decimal import Decimal
+
+fuel_lvl: dict[str, str] = { '0.0': "E", '0.25': "25%", '0.5': "50%", '0.75': "75%", '1.0': "F" }
+mask: str = "\d+(\.{1}\d+)"
+
+def main():
+    input_splitter()
+
+def input_splitter():
+    while True:
+        try:
+            x, y =  input("Fraction: ").split('/')
+
+            if(re.match(mask, x) is not None and re.match(mask, y) is not None):
+                raise ValueError
+            
+            if(int(x) < 5 and (int(y) == 2 or int(y) == 4)):
+                print(fuel_lvl[str(int(x)/int(y))])
+                break
+
+        except ValueError:
+            pass
+        except ZeroDivisionError:
+            pass
+
+if __name__ == "__main__":
+    main()
+
+
+# https://stackoverflow.com/questions/19965018/python-decimal-checking-if-integer
