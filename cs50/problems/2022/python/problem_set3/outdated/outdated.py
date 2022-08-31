b85e8357e05267ee36b6c74cfd74527a65ecaa58
Ezequiel Augusto Stanganelli <ezequielstanganelli@gmail.com>
2022-08-31 12:05:11 +0000

    Problem set3 finished

---
+++ cs50/problems/2022/python/problem_set3/outdated/outdated.py
@@ -0,0 +1,20 @@
+month_list: list[str] = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+]
+
+def main():
+
+
+if __name__ == "__main__":
+    main()
